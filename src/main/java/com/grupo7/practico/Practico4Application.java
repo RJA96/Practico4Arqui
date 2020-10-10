e2ce5742ef760d765540ca0f925e9616380f82ab
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2020-10-10 17:08:44 -0300

    first commit

---
+++ src/main/java/com/grupo7/practico/Practico4Application.java
@@ -0,0 +1,13 @@
+package com.grupo7.practico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practico4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practico4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo7/practico/Practico4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practico4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
